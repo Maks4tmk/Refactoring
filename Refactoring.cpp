71ca5ddb900f7a080fdcc8608e3a71345dff0c78
Maksim <maks4tmk5423@yandex.ru>
2024-07-17 01:10:56 +0300

    Ready tests

--- Refactoring.cpp
+++ Refactoring.cpp
@@ -3,64 +3,284 @@
 
 #include <iostream>
 #include "shape.h"
+#include "transform.h"
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/catch_session.hpp"
 
 
-Shape Line(0, 1, 2, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-Shape Square(1, 1, 2, 0, 3, 4, 0, 5, 6, 0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-Shape Cube(2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
-Shape Circle(3, 1, 2, 3, 4);
-Shape Cylinder(4, 0, 0, 0, 1);
+Shape shLine(0, 1, 2, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+Shape shSquare(1, 1, 2, 0, 3, 4, 0, 5, 6, 0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+Shape shCube(2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
+Shape shCircle(3, 1, 2, 3, 0);
+Shape shCylinder(4, 1, 2, 3, 4);
 
 
-TEST_CASE("Проверка Line") {
-	REQUIRE(Line.type == 0);
-	REQUIRE(Line.x1 == 1);
-	REQUIRE(Line.y1 == 2);
-	REQUIRE(Line.x2 == 3);
-	REQUIRE(Line.y2 == 4);
-	REQUIRE(Line.square == 0);
-	REQUIRE(Line.volume == 0);
+TEST_CASE("Проверка класс Shape") {
+
+	SECTION("Тест Line")
+	{
+		CHECK(shLine.type == 0);
+		CHECK(shLine.x1 == 1);
+		CHECK(shLine.y1 == 2);
+		CHECK(shLine.x2 == 3);
+		CHECK(shLine.y2 == 4);
+
+		CHECK(shLine.square == 0);
+		CHECK(shLine.volume == 0);
+	}
+
+
+	SECTION("Тест Square")
+	{
+		CHECK(shSquare.type == 1);
+		CHECK(shSquare.x1 == 1);
+		CHECK(shSquare.y1 == 2);
+		CHECK(shSquare.x2 == 3);
+		CHECK(shSquare.y2 == 4);
+		CHECK(shSquare.x3 == 5);
+		CHECK(shSquare.y3 == 6);
+		CHECK(shSquare.x4 == 7);
+		CHECK(shSquare.y4 == 8);
+
+		CHECK(shSquare.square == 4);
+		CHECK(shSquare.volume == 0);
+	}
+
+
+	SECTION("Тест Cube")
+	{
+		CHECK(shCube.type == 2);
+		CHECK(shCube.x1 == 1); CHECK(shCube.x2 == 4); CHECK(shCube.x3 == 7);
+		CHECK(shCube.y1 == 2); CHECK(shCube.y2 == 5); CHECK(shCube.y3 == 8);
+		CHECK(shCube.z1 == 3); CHECK(shCube.z2 == 6); CHECK(shCube.z3 == 9);
+
+		CHECK(shCube.x4 == 10); CHECK(shCube.x5 == 13); CHECK(shCube.x6 == 16);
+		CHECK(shCube.y4 == 11); CHECK(shCube.y5 == 14); CHECK(shCube.y6 == 17);
+		CHECK(shCube.z4 == 12); CHECK(shCube.z5 == 15); CHECK(shCube.z6 == 18);
+
+		CHECK(shCube.x7 == 19); CHECK(shCube.x8 == 22);
+		CHECK(shCube.y7 == 20); CHECK(shCube.y8 == 23);
+		CHECK(shCube.z7 == 21); CHECK(shCube.z8 == 24);
+
+		CHECK(shCube.square == 54);
+		CHECK(shCube.volume == 27);
+	}
+
+	SECTION("Тест Cylinder")
+	{
+		CHECK(shCylinder.type == 4);
+		CHECK(shCylinder.x1 == 1);
+		CHECK(shCylinder.y1 == 2);
+		CHECK(shCylinder.radius == 3);
+		CHECK(shCylinder.height == 4);
+
+		CHECK(shCylinder.square == 52.274333882308142);
+		CHECK(shCylinder.volume == 113.09733552923255);
+	}
+
+	SECTION("Тест Circle")
+	{
+		CHECK(shCircle.type == 3);
+		CHECK(shCircle.x1 == 1);
+		CHECK(shCircle.y1 == 2);
+		CHECK(shCircle.radius == 3);
+
+		CHECK(shCircle.square == 28.274333882308138);
+		CHECK(shCircle.volume == 0);
+	}
 }
 
-TEST_CASE("Проверка Square") {
-	REQUIRE(Square.type == 1);
-	REQUIRE(Square.x1 == 1);
-	REQUIRE(Square.y1 == 2);
-	REQUIRE(Square.x2 == 3);
-	REQUIRE(Square.y2 == 4);
-	REQUIRE(Square.x3 == 5);
-	REQUIRE(Square.y3 == 6);
-	REQUIRE(Square.x4 == 7);
-	REQUIRE(Square.y4 == 8);
+
+transform trLine(shLine);
+transform trSquare(shSquare);
+transform trCube(shCube);
+
+TEST_CASE("Проверка класс transform") {
+	SECTION("Тест trLine")
+	{
+		SECTION("shift") {
+			Shape exLine = trLine.shift(1, 2, 3);
+
+			CHECK(exLine.x1 == 2);
+			CHECK(exLine.y1 == 4);
+			CHECK(exLine.x2 == 4);
+			CHECK(exLine.y2 == 6);
+		}
+
+		SECTION("scaleX") {
+			Shape exLine = trLine.scaleX(2);
+
+			CHECK(exLine.x1 == 4);
+			CHECK(exLine.x2 == 8);
+
+		}
+
+		SECTION("scaleY") {
+			Shape exLine = trLine.scaleY(3);
+
+			CHECK(exLine.y1 == 12);
+			CHECK(exLine.y2 == 18);
+		}
+		
+		SECTION("scaleZ") {
+			Shape exLine = trLine.scaleZ(4);
+
+			CHECK(exLine.z1 == 0);
+			CHECK(exLine.z2 == 0);
+
+		}
+
+		SECTION("scale") {
+			Shape exLine = trLine.scale(5);
+
+			CHECK(exLine.x1 == 0);
+			CHECK(exLine.y1 == 2);
+			CHECK(exLine.x2 == 1);
+			CHECK(exLine.y2 == 3);
+		}
+	}
+
+
+
+
+	SECTION("Тест trSquare")
+	{
+		SECTION("shift") {
+			Shape exSquare = trSquare.shift(1, 2, 3);
+
+			CHECK(exSquare.x1 == 2);
+			CHECK(exSquare.y1 == 4);
+			CHECK(exSquare.x2 == 4);
+			CHECK(exSquare.y2 == 6);
+			CHECK(exSquare.x3 == 6);
+			CHECK(exSquare.y3 == 8);
+			CHECK(exSquare.x4 == 8);
+			CHECK(exSquare.y4 == 10);
+		}
+
+		SECTION("scaleX") {
+			Shape exSquare = trSquare.scaleX(2);
+
+			CHECK(exSquare.x1 == 4);
+			CHECK(exSquare.x2 == 8);
+			CHECK(exSquare.x3 == 12);
+			CHECK(exSquare.x4 == 16);
+
+		}
+
+		SECTION("scaleY") {
+			Shape exSquare = trSquare.scaleY(3);
+
+			CHECK(exSquare.y1 == 12);
+			CHECK(exSquare.y2 == 18);
+			CHECK(exSquare.y3 == 24);
+			CHECK(exSquare.y4 == 30);
+		}
+
+		SECTION("scaleZ") {
+			Shape exSquare = trSquare.scaleZ(4);
+
+			CHECK(exSquare.z1 == 0);
+			CHECK(exSquare.z2 == 0);
+			CHECK(exSquare.z3 == 0);
+			CHECK(exSquare.z4 == 0);
+
+		}
+
+		SECTION("scale") {
+			Shape exSquare = trSquare.scale(5);
+
+			CHECK(exSquare.x1 == 0);
+			CHECK(exSquare.y1 == 2);
+			CHECK(exSquare.x2 == 1);
+			CHECK(exSquare.y2 == 3);
+			CHECK(exSquare.x3 == 2);
+			CHECK(exSquare.y3 == 4);
+			CHECK(exSquare.x4 == 3);
+			CHECK(exSquare.y4 == 6);
+		}
+	}
+
+
+
+
+
+	SECTION("Тест trCube")
+	{
+		SECTION("shift") {
+			Shape exCube = trCube.shift(1, 2, 3);
+
+			CHECK(exCube.x1 == 2); CHECK(exCube.x2 == 5); CHECK(exCube.x3 == 8);
+			CHECK(exCube.y1 == 4); CHECK(exCube.y2 == 7); CHECK(exCube.y3 == 10);
+			CHECK(exCube.z1 == 6); CHECK(exCube.z2 == 9); CHECK(exCube.z3 == 12);
+
+			CHECK(exCube.x4 == 11); CHECK(exCube.x5 == 14); CHECK(exCube.x6 == 17);
+			CHECK(exCube.y4 == 13); CHECK(exCube.y5 == 16); CHECK(exCube.y6 == 19);
+			CHECK(exCube.z4 == 15); CHECK(exCube.z5 == 18); CHECK(exCube.z6 == 21);
+
+			CHECK(exCube.x7 == 20); CHECK(exCube.x8 == 23);
+			CHECK(exCube.y7 == 22); CHECK(exCube.y8 == 25);
+			CHECK(exCube.z7 == 24); CHECK(exCube.z8 == 27);
+		}
+
+		SECTION("scaleX") {
+			Shape exCube = trCube.scaleX(2);
+
+			CHECK(exCube.x1 == 4);
+			CHECK(exCube.x2 == 10);
+			CHECK(exCube.x3 == 16);
+			CHECK(exCube.x4 == 22);
+			CHECK(exCube.x5 == 28);
+			CHECK(exCube.x6 == 34);
+			CHECK(exCube.x7 == 40);
+			CHECK(exCube.x8 == 46);
+
+		}
+
+		SECTION("scaleY") {
+			Shape exCube = trCube.scaleY(3);
+
+			CHECK(exCube.y1 == 12);
+			CHECK(exCube.y2 == 21);
+			CHECK(exCube.y3 == 30);
+			CHECK(exCube.y4 == 39);
+			CHECK(exCube.y5 == 48);
+			CHECK(exCube.y6 == 57);
+			CHECK(exCube.y7 == 66);
+			CHECK(exCube.y8 == 75);
+		}
+
+		SECTION("scaleZ") {
+			Shape exCube = trCube.scaleZ(4);
+
+			CHECK(exCube.z1 == 24);
+			CHECK(exCube.z2 == 9);
+			CHECK(exCube.z3 == 48);
+			CHECK(exCube.z4 == 60);
+			CHECK(exCube.z5 == 72);
+			CHECK(exCube.z6 == 84);
+			CHECK(exCube.z7 == 96);
+			CHECK(exCube.z8 == 108);
+
+		}
+
+		SECTION("scale") {
+			Shape exCube = trCube.scale(5);
+
+			CHECK(exCube.x1 == 0); CHECK(exCube.x2 == 2); CHECK(exCube.x3 == 3);
+			CHECK(exCube.y1 == 2); CHECK(exCube.y2 == 16); CHECK(exCube.y3 == 6);
+			CHECK(exCube.z1 == 4); CHECK(exCube.z2 == 1); CHECK(exCube.z3 == 9);
+
+			CHECK(exCube.x4 == 4); CHECK(exCube.x5 == 5); CHECK(exCube.x6 == 6);
+			CHECK(exCube.y4 == 7); CHECK(exCube.y5 == 9); CHECK(exCube.y6 == 11);
+			CHECK(exCube.z4 == 12); CHECK(exCube.z5 == 14); CHECK(exCube.z6 == 16);
+
+			CHECK(exCube.x7 == 8); CHECK(exCube.x8 == 9);
+			CHECK(exCube.y7 == 13); CHECK(exCube.y8 == 15);
+			CHECK(exCube.z7 == 19); CHECK(exCube.z8 == 21);
+		}
+	}
 }
-
-TEST_CASE("Проверка Cube") {
-	REQUIRE(Cube.type == 2);
-	REQUIRE(Cube.x1 == 1); REQUIRE(Cube.x2 == 4); REQUIRE(Cube.x3 == 7);
-	REQUIRE(Cube.y1 == 2); REQUIRE(Cube.y2 == 5); REQUIRE(Cube.y3 == 8);
-	REQUIRE(Cube.z1 == 3); REQUIRE(Cube.z2 == 6); REQUIRE(Cube.z3 == 9);
-
-	REQUIRE(Cube.x4 == 10); REQUIRE(Cube.x5 == 13); REQUIRE(Cube.x6 == 16);
-	REQUIRE(Cube.y4 == 11); REQUIRE(Cube.y5 == 14); REQUIRE(Cube.y6 == 17);
-	REQUIRE(Cube.z4 == 12); REQUIRE(Cube.z5 == 15); REQUIRE(Cube.z6 == 18);
-
-	REQUIRE(Cube.x7 == 19); REQUIRE(Cube.x8 == 22);
-	REQUIRE(Cube.y7 == 20); REQUIRE(Cube.y8 == 23);
-	REQUIRE(Cube.z7 == 21); REQUIRE(Cube.z8 == 24);
-}
-
-TEST_CASE("Проверка Circle") {
-	REQUIRE(Circle.type == 3);
-	REQUIRE(Circle.x1 == 1);
-	REQUIRE(Circle.y1 == 2);
-	REQUIRE(Circle.radius == 3);
-	REQUIRE(Circle.height == 4);
-	
-}
-
-
 
 
 
---
+++ git_reload.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Ready tests"
+git push -u origin main
